926b32aaadea2324d50c16a90bec960ee4a2b636
Israel <isrmicha@gmail.com>
2017-11-21 15:12:44 -0200

    huj

--- shareArr.js
+++ shareArr.js
@@ -1,9 +1,11 @@
 const cluster = require('cluster');
 const http = require('http');
 const numCPUs = require('os').cpus().length;
+var tempNum = numCPUs;
 var fs = require('fs');
 if (cluster.isMaster) { //MASTER
     var arrBuffer = [];
+    var arrTemp = [];
     for (let i = 0; i <100; i++) {
             arrBuffer.push(i);
          }
@@ -11,13 +13,23 @@ if (cluster.isMaster) { //MASTER
     for (let i = 0; i < numCPUs; i++) {
         var worker = cluster.fork();
         worker.send({arr : arrBuffer.splice(0,Math.ceil(tempLength/numCPUs)),index : i});
-        console.log(arrBuffer.length);
+        worker.on('message',(msg)=>{
+            arrTemp = arrTemp.concat(msg);
+         })
+        worker.on('exit',()=>{
+            console.log("Worker Morto");
+            tempNum--;
+            console.log(tempNum);
+            if(!tempNum){
+                console.log('FIM');
+                console.log(arrTemp.sort((a,b)=>parseInt(a)-parseInt(b)));
+            }
+        })
     }
-
+    // console.log(Object.keys(cluster.workers));
 } else { //SLAVE
     process.on('message',(msg)=>{
-        console.log(msg.arr);
-        fs.writeFileSync('texto'+msg.index,msg.arr.join());
+        process.send(msg.arr);
+        process.exit();
     })
-
 }
